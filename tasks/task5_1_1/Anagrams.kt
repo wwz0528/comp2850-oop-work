4422bf1aa089f3549154c116d69692fa199268d6
wwz0528 <czvr7309@leeds.ac.uk>
2025-10-07 13:48:23 +0000

    1.1

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,16 @@
-// Task 5.1.1: anagram checking using a function
+fun anagrams(first: String, second: String): Boolean {
+    if (first.length != second.length) {
+        return false
+    }
+    val firstChars = first.lowercase().toList().sorted()
+    val secondChars = second.lowercase().toList().sorted()
+    return firstChars == secondChars
+}
+
+fun main() {
+    print("Please enter 2 inputs: ")
+    val first = readln()
+    val second = readln()
+    val output = anagrams(first, second)
+    println("The answer is $output")
+}
--- tasks/task5_1_2/Die.kt
+++ tasks/task5_1_2/Die.kt
@@ -1,1 +1,12 @@
-// Task 5.1.2: die rolling simulation
+import kotlin.random.Random
+
+fun rollDie(sides: Int){
+    if (sides in setOf(4,6,,10,12,20)) {
+        println("Roolling a d$sides...")
+        val result = random.nextInt(1, sides + 1)
+        println("You rolled $reslt")
+    }
+    else{
+        println("Error: cannot have a $sides-sided die")
+    }
+}
